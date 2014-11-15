a2a69c6f716600dafecf228949c82910fb199f78
angryhussord <angryhussord@outlook.com>
2014-11-15 07:31:20 -0800

    Adding initial files.

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'launchbox' 
+$installerType = 'exe' 
+$url = 'http://www.launchbox-app.com/index.php?sdmon=wp-content/downloads/LaunchBox-3.7-Setup.exe' 
+$silentArgs = '/s' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
