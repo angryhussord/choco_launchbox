5b37c10c009e3d1ede83741cdcb38b4fb4b4a772
angryhussord <angryhussord@outlook.com>
2015-07-31 16:02:12 -0700

    4.1 release

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'launchbox' 
 $installerType = 'exe' 
-$url = 'http://www.launchbox-app.com/index.php?sdmon=wp-content/downloads/LaunchBox-3.7-Setup.exe' 
+$url = 'http://www.launchbox-app.com/index.php?sdmon=wp-content/downloads/LaunchBox-4.1-Setup.exe' 
 $silentArgs = '/s' 
 $validExitCodes = @(0) 
 
