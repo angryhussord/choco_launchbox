b12cd487a56b1a33d4ff9ee5179d2d217570882f
Patrick Hufford <angryhussord@outlook.com>
2015-12-02 06:43:27 -0800

    Updating for 5.5 release

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'launchbox' 
 $installerType = 'exe' 
 $url = 'http://www.launchbox-app.com/index.php?sdmon=wp-content/downloads/LaunchBox-4.1-Setup.exe' 
-$silentArgs = '/s' 
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
 $validExitCodes = @(0) 
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
